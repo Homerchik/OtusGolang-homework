9920bd4fea75790100d86885e238f0bcabcaae89
Yevhenii.Hulkov <Yevhenii.Hulkov@acronis.com>
2024-10-11 21:13:57 +0200

    fixed depguard

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,14 +8,13 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-
 func processShieldCase(s string, startIdx int, result *strings.Builder) (int, error) {
-	nextRune := rune(s[startIdx + 1])
-	if startIdx + 2 < len(s) {
-		afterNextRune := rune(s[startIdx + 2])
+	nextRune := rune(s[startIdx+1])
+	if startIdx+2 < len(s) {
+		afterNextRune := rune(s[startIdx+2])
 		switch {
 		case unicode.IsDigit(afterNextRune):
-			result.WriteString(strings.Repeat(string(nextRune), int(afterNextRune - '0')))
+			result.WriteString(strings.Repeat(string(nextRune), int(afterNextRune-'0')))
 			return 3, nil
 		case unicode.IsLetter(afterNextRune) || afterNextRune == '\\':
 			result.WriteString(string(nextRune))
@@ -29,21 +28,20 @@ func processShieldCase(s string, startIdx int, result *strings.Builder) (int, er
 	}
 }
 
-
 func Unpack(s string) (string, error) {
 	fstCharIdx := 0
 	result := strings.Builder{}
 	for {
-		if fstCharIdx > len(s) - 2 {
+		if fstCharIdx > len(s)-2 {
 			break
 		}
-		fstRune, nextRune := rune(s[fstCharIdx]), rune(s[fstCharIdx + 1])
+		fstRune, nextRune := rune(s[fstCharIdx]), rune(s[fstCharIdx+1])
 		increment := 0
-		switch  {
+		switch {
 		case fstRune == '\\' && (nextRune == '\\' || unicode.IsDigit(nextRune)):
 			if inc, err := processShieldCase(s, fstCharIdx, &result); err != nil {
 				return "", err
- 			} else {
+			} else {
 				increment = inc
 			}
 		case fstRune == '\\' && !(nextRune == '\\' || unicode.IsDigit(nextRune)):
@@ -53,15 +51,15 @@ func Unpack(s string) (string, error) {
 			increment = 1
 		case unicode.IsLetter(fstRune) && unicode.IsDigit(nextRune):
 			increment = 2
-			result.WriteString(strings.Repeat(string(fstRune), int(nextRune - '0')))
+			result.WriteString(strings.Repeat(string(fstRune), int(nextRune-'0')))
 		default:
 			return "", ErrInvalidString
 		}
 		fstCharIdx += increment
 	}
 	if fstCharIdx < len(s) {
-		lastRune := rune(s[len(s) - 1])
-		if lastRune == '\\' || !unicode.IsLetter(lastRune)  {
+		lastRune := rune(s[len(s)-1])
+		if lastRune == '\\' || !unicode.IsLetter(lastRune) {
 			return "", ErrInvalidString
 		}
 		result.WriteByte(s[fstCharIdx])
